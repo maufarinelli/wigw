a10894766c1ec350882682c61b35146aaa8fb024
Farinelli, Mauricio <Mauricio.Farinelli@Kronos.com>
2015-07-10 15:58:59 -0400

    Dashboard enable to intantiate multiple teams calculators

--- modules/dashboard/dashboard-controller.js
+++ modules/dashboard/dashboard-controller.js
@@ -4,7 +4,13 @@
             urlTeam: 'team1',
             urlExponent: 'exponent'
         };
-        var calculator = new CalculatorComponent(team1Config);
+        var calculator1 = new CalculatorComponent(team1Config);
+
+        var team2Config = {
+            urlTeam: 'team2',
+            urlExponent: 'exponent'
+        };
+        var calculator2 = new CalculatorComponent(team2Config);
     }
 
     angular.module('dashboard')
