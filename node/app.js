4e533059b2e094328dc85269c61e1d9797cbb6f8
Mauricio Farinelli <maufarinelli@gmail.com>
2015-08-12 22:55:51 -0400

    Addnig a test for MongoDB connection

--- node/app.js
+++ node/app.js
@@ -16,3 +16,14 @@ app.get('/exponent/', function(req, res) {
 });
 
 app.listen(9090);
+
+// Mongo DB First test
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
